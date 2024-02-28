acf5f4ef16bb3e999ca2ba4e69684c1cff051f66
Alisha092 <ali1382.emp@gmail.com>
2024-02-28 17:57:15 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.addEventListener('DOMContentLoaded', () => {
+const canvas = document.getElementById('canvas');
+const startBtn = document.getElementById('start');
+const score = document.getElementById('score');
+
+const map = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+startBtn.addEventListener('click', () => {
+    console.log('clicked');
+});
