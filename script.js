e19794dde52ca65f0c6095287f3a1bc041d06920
Alisha092 <ali1382.emp@gmail.com>
2024-02-28 18:21:02 +0330

    correct some mistakes

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-// document.addEventListener('DOMContentLoaded', () => {
-const canvas = document.getElementById('canvas');
-const startBtn = document.getElementById('start');
-const score = document.getElementById('score');
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('canvas');
+    const startBtn = document.getElementById('start');
+    const scoreSpan = document.getElementById('score');
+    let gameOver = false;
+    let isWinner = false;
+    let score = 0;
 
-const map = [
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-];
+    const map = [
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    ];
 
-startBtn.addEventListener('click', () => {
-    console.log('clicked');
+    function createMap(map) {
+        for (let i = 0; i < map.length; i++) {
+            const cell = document.createElement('div');
+            if (i === 27) {
+                cell.classList.add('snake');
+            } else {
+                cell.classList.add('cell');
+            }
+            canvas.appendChild(cell);
+        }
+    };
+
+    function keyDownHandler(event) {
+        console.log(event);
+        gameOver = true;
+    }
+
+    function gameStart() {
+        createMap(map);
+        while (!gameOver && !isWinner) {
+            document.addEventListener('keydown', keyDownHandler);
+        }
+    }
+
+    startBtn.addEventListener('click', gameStart);
 });
